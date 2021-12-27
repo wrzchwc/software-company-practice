0a49a4f84a0b2ee0b916eb9b85470229c7d09169
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2021-12-27 18:44:03 +0100

    Cleanup and icon change

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {AppBar, useScrollTrigger} from "@material-ui/core";
-import {Toolbar} from "@material-ui/core";
-import {Typography} from "@material-ui/core";
+import {AppBar, makeStyles, Toolbar, useScrollTrigger} from "@material-ui/core";
+import logo from "../../assets/logo.svg"
 
 const ElevationScroll = props => {
     const {children} = props;
@@ -16,17 +15,31 @@ const ElevationScroll = props => {
     });
 }
 
-const Header = props => {
-    return (
-        <ElevationScroll>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <Typography variant="h3">Arc Development</Typography>
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
-    );
+const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: "3em"
+    },
+    logo: {
+        height: "7em"
+    }
+}))
 
+const Header = props => {
+    const classes = useStyles();
+
+    return (
+        <>
+            <ElevationScroll>
+                <AppBar position="fixed">
+                    <Toolbar disableGutters>
+                        <img src={logo} alt="company logo" className={classes.logo}/>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin}/>
+        </>
+    );
 }
 
 export default Header;
