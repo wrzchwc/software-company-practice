8c3d6463ce33016876d92dff859e7b43b4a17f91
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-01-07 21:17:27 +0100

    Basic footer styling

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,14 +1,25 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core";
+import footerAdornment from "../../assets/Footer Adornment.svg";
 
 const useStyles = makeStyles(theme => ({
     footer: {
         backgroundColor: theme.palette.common.blue,
-        width: "100%"
+        position: "relative",
+        width: "100%",
+        zIndex: 1302
+    },
+    adornment: {
+        width: "25em",
+        verticalAlign: "bottom"
     }
 }))
 
 export const Footer = () => {
     const classes = useStyles();
-    return <footer className={classes.footer}>footer</footer>;
+    return (
+        <footer className={classes.footer}>
+            <img className={classes.adornment} src={footerAdornment} alt="black decorative slash"/>
+        </footer>
+    );
 }
